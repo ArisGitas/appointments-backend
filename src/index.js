ce11f46cb88a50d685ce8c7b51a6ecfca4a50958
ArisGitas <141219898+ArisGitas@users.noreply.github.com>
2025-06-03 09:42:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3333;
+const port = process.env.PORT || 3333;
 
-app.use(express.json());
-
-app.get('/hello', (req, res) => {
-  res.send('Γεια σου από το backend!');
+app.get('/', (req, res) => {
+  res.send('✅ Ο server δουλεύει!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
